a6b5312ef16469c3693963eb965ba66bb61ca592
Vic <victoria.ad@gmail.com>
2021-10-08 12:26:59 -0300

    input text

--- src/view/store/index.tsx
+++ src/view/store/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextInput from '../../components/textInput'
 
 const Store: React.FC = () => {
+    const [input, setInput] = useState('')
+
     return (
         <div>
             <h1>Loja</h1>
+            <form>
+                <TextInput placeholder='Sou um input teste' value={input} onChange={input => setInput(input)} />
+            </form>
         </div>
     )
 }
