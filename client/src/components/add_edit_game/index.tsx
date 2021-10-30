49eb0a2406efd79b1b71d6facd433d49834876bf
Vic <victoria.ad@gmail.com>
2021-10-30 17:18:32 -0300

    Feat: add and edit game

---
+++ client/src/components/add_edit_game/index.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { TGame } from "../../types/TGame"
+import DefaultButton from "../default_button"
+import InfoPage from '../info_page'
+import TextInput from "../textInput"
+import TextArea from '../text_area'
+import './styles.css'
+
+interface AddEditGameProps {
+    gameItem?: TGame
+    onReturn: () => void
+}
+
+const AddEditGame: React.FC<AddEditGameProps> = ({gameItem, onReturn}) => {
+    const [game, setGame] = useState(gameItem? gameItem : {
+        title: '',
+        description: '',
+        developer: '',
+        price: 0.00,
+        categories: []
+    })
+    const [isOperationFinnished, setIsOperationFinnished] = useState(false)
+
+    return (
+        <>
+            {isOperationFinnished ?
+                <InfoPage infoText='Sua solicitação foi enviada com sucesso' buttonText='Voltar ao menu principal' onClick={onReturn} />
+                :
+                <div className="add-edit-game page">
+                    <h1 className="page-title">Adicionar jogo</h1>
+                    <h2 className="page-subtitle">Informações do jogo</h2>
+                    <TextInput hasLabel text="Nome" value={game.title} onChange={newTitle => setGame({...game, title: newTitle})} />
+                    <TextInput hasLabel text="Preço" value={game.price.toString()} onChange={newPrice => setGame({...game, price: parseInt(newPrice)})} />
+                    <TextInput hasLabel text="Desenvolvedor" value={game.developer} onChange={newDeveloper => setGame({...game, developer: newDeveloper})} />
+                    <TextArea labelText="Descrição" value={game.description} onChange={newDescription => setGame({...game, description: newDescription})}/>
+                    <DefaultButton text="Fazer pedido de adição" colorClass="primary" onClick={() => setIsOperationFinnished(true)}/>
+                </div>
+            }
+        </>
+    )
+}
+export default AddEditGame
--- client/src/view/game_management/index.tsx
+++ client/src/view/game_management/index.tsx
@@ -1,25 +1,35 @@
 import React, { useState } from 'react'
+import AddEditGame from '../../components/add_edit_game'
 import DefaultButton from '../../components/default_button'
 import DeleteGame from '../../components/delete_game'
 import DevGameCard from '../../components/dev_game_card'
 import TextInput from '../../components/textInput'
 import { gamesListPopulate } from '../../constant/content'
+import { TGame } from '../../types/TGame'
 import './styles.css'
+
+enum GameAction {
+    LIST_VIEW,
+    DELETE,
+    ADD,
+    EDIT
+}
 
 const GameManagement: React.FC = () => {
     const [beginPeriodValue, setBeginPediodValue] = useState('')
     const [endPediodValue, setEndPeriodValue] = useState('')
-    const [deleteGame, setDeleteGame] = useState(false)
-    const [gameToDelete, setGameToDelete] = useState('')
+    const [gameAction, setGameAction] = useState(GameAction.LIST_VIEW)
+    const [gameToHandle, setGameToHandle] = useState<TGame | undefined >(undefined)
     const [gamesOnRequest, setGamesOnRequest] = useState<string[]>([])
 
-    const handleEditGame = () => {
-        console.log('Editar jogo')
+    const handleEditGame = (game: TGame) => {
+        setGameToHandle(game)
+        setGameAction(GameAction.EDIT)
     }
 
-    const handleDeleteGame = (gameTitle: string) => {
-        setGameToDelete(gameTitle)
-        setDeleteGame(true)
+    const handleDeleteGame = (game: TGame) => {
+        setGameToHandle(game)
+        setGameAction(GameAction.DELETE)
     }
 
     const onRequestOpen = (gameTitle: string) => {
@@ -27,8 +37,8 @@ const GameManagement: React.FC = () => {
     }
 
     const returnToMainPage = () => {
-        setDeleteGame(false)
-        setGameToDelete('')
+        setGameAction(GameAction.LIST_VIEW)
+        setGameToHandle(undefined)
     }
 
     const handleRenovateGame = () => {
@@ -37,8 +47,14 @@ const GameManagement: React.FC = () => {
 
     return (
         <div className="page dev_game_management">
-            {deleteGame ?
-                <DeleteGame gameTitle={gameToDelete} onCancel={() => setDeleteGame(false)} onDelete={() => onRequestOpen(gameToDelete)} onReturn={() => returnToMainPage()} />
+            {gameAction == GameAction.DELETE ?
+                <DeleteGame gameTitle={gameToHandle!.title} onCancel={() => setGameAction(GameAction.LIST_VIEW)} onDelete={() => onRequestOpen(gameToHandle!.title)} onReturn={() => returnToMainPage()} />
+                :
+                gameAction == GameAction.EDIT ?
+                <AddEditGame gameItem={gameToHandle} onReturn={() => returnToMainPage()}/>
+                :
+                gameAction == GameAction.ADD ?
+                <AddEditGame onReturn={() => returnToMainPage()}/>
                 :
                 <>
                     <h1 className="page-title">Jogos</h1>
@@ -50,8 +66,8 @@ const GameManagement: React.FC = () => {
                     </div>
                     <h2 className="page-subtitle">Meus Jogos</h2>
                     <div className="dev_game_management__game_list">
-                        {gamesListPopulate.map((game, index) => <DevGameCard key={index} game={game} handleEditGame={() => handleEditGame()} handleDeleteGame={() => handleDeleteGame(game.title)} handleRenovateGame={() => handleRenovateGame()} onRequest={gamesOnRequest.includes(game.title)} />)}
-                        <DefaultButton text="Adicionar jogo" colorClass="primary" onClick={() => console.log('adicionar jogo')} />
+                        {gamesListPopulate.map((game, index) => <DevGameCard key={index} game={game} handleEditGame={() => handleEditGame(game)} handleDeleteGame={() => handleDeleteGame(game)} handleRenovateGame={() => handleRenovateGame()} onRequest={gamesOnRequest.includes(game.title)} />)}
+                        <DefaultButton text="Adicionar jogo" colorClass="primary" onClick={() => setGameAction(GameAction.ADD)} />
                     </div>
                 </>
             }
