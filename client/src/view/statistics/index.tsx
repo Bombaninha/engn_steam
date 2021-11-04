529842b3ba1bbb501fe1e17100fe610022325bb0
marcoantonioaav <56897818+marcoantonioaav@users.noreply.github.com>
2021-11-04 18:06:17 -0300

    Admin Statistics

--- client/src/view/statistics/index.tsx
+++ client/src/view/statistics/index.tsx
@@ -1,47 +1,25 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
-import { Modal } from '../../components/Modal/modal';
-
-import { useGame } from '../../hooks/useGame';
-
-const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-`;
-
-const Button = styled.button`
-    min-width: 100px;
-    padding: 16px 32px;
-    border-radius: 4px;
-    border: none;
-    background: #141414;
-    color: #FFF;
-    font-size: 24px;
-    cursor: pointer;
-`;
+import DefaultButton from '../../components/default_button';
+import SelectInput from '../../components/select_input';
+import TextInput from '../../components/textInput';
+import './styles.css'
 
 const Statistics: React.FC = () => {
-    const [ showModal, setShowModal ] = useState(false);
+    const [reportTypeValue, setReportTypeValue] = useState('Vendas de jogos')
+    const [beginPeriodValue, setBeginPediodValue] = useState('')
+    const [endPediodValue, setEndPeriodValue] = useState('')
 
-    const games = useGame();
-    console.log(games);
-
-    const openModal = () => {
-        setShowModal(prev => !prev);
-    }
-
+    
     return (
-        <>
-        <div>
-            { games }
+        <div className="page">
+            <h1 className="page-title">Estatísticas</h1>
+            <div className="statistics">
+                <SelectInput value={reportTypeValue} label='Tipo de relatório' identification='report-type' options={[{ value: 'sales', label: 'Vendas de jogos' }, { value: 'tickets', label: 'Tickets resolvidos' }]} onChange={select => setReportTypeValue(select)} />
+                <TextInput text="Início do período" value={beginPeriodValue} onChange={newBeginPeriod => setBeginPediodValue(newBeginPeriod)} hasLabel />
+                <TextInput text="Fim do período" value={endPediodValue} onChange={newEndPeriod => setEndPeriodValue(newEndPeriod)} hasLabel />
+                <DefaultButton text="Gerar relatório" colorClass="primary" onClick={() => console.log('gerar relatorio')} />
+            </div>
         </div>
-            <Container>
-                <Button onClick={openModal}>I'm a modal</Button>
-                <Modal showModal={showModal} setShowModal={setShowModal}/>
-            </Container>
-        </>
     )
 }
 export default Statistics
